25091aff456436cc7d9dd06fb3074c4442dabd8b
Jacob <jacob-messinger@hotmail.com>
2020-04-22 15:21:00 +0200

    Create and basic design for Detailed Company page

--- DateIt/app/src/main/java/com/example/dateit/MainActivity.java
+++ DateIt/app/src/main/java/com/example/dateit/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.dateit;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -14,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TextView companyName = (TextView) findViewById(R.id.companyName);
+     //   companyName.setText("wwww");'
+
+        Button goToCompanyBtn = (Button) findViewById(R.id.goToCompanyBtn);
+
+
+     /*   goToCompanyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View companyView) {
+                Navigation.findNavController(companyView).navigate(R.id.companyDetails);
+            }
+        });
+      */
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
---
+++ DateIt/app/src/main/java/com/example/dateit/ui/home/companyDetails.java
@@ -0,0 +1,35 @@
+package com.example.dateit.ui.home;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dateit.R;
+
+public class companyDetails extends Fragment {
+
+    public static companyDetails newInstance() {
+        return new companyDetails();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.company_details_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // TODO: Use the ViewModel
+    }
+
+}
