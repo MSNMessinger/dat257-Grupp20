0c260f99aa2c44bcc9de127aecadda972f0005b0
Tina Samimian <tina.samimian@gmail.com>
2020-04-22 21:05:58 +0200

    added companies to visit

--- DateIt/app/src/main/java/com/example/dateit/ui/myPage/NotificationsFragment.java
+++ DateIt/app/src/main/java/com/example/dateit/ui/myPage/NotificationsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,13 +25,19 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+
+        String[] companiesToVisit = {"SKF", "ericsson"};
+        ListView listToVisit = (ListView)root.findViewById(R.id.listToVisit);
+        ArrayAdapter<String> companyAdapter = new ArrayAdapter<~>(getActivity(),android.R.id.simple_list_item_1,companiesToVisit);
+        listToVisit.setAdapter(companyAdapter);
+
+        /*final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
