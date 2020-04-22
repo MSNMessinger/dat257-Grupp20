bd513693a1a9275f72f0ca9e2b4d2f2d7f5b18a6
Tina Samimian <tina.samimian@gmail.com>
2020-04-22 23:00:37 +0200

    added base for 2 lists in my pages

--- DateIt/app/src/main/java/com/example/dateit/ui/notifications/NotificationsFragment.java
+++ DateIt/app/src/main/java/com/example/dateit/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,13 +25,23 @@ public class NotificationsFragment extends Fragment {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+        /*final TextView textView = root.findViewById(R.id.titleToVisit);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+
+        String[] namesOfCompaniesToVisit = {"Volvo", "Ericsson", "Brocoli"};
+        ListView companyList = (ListView)root.findViewById(R.id.listToVisit);
+        ArrayAdapter<String> companyToVisitAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,namesOfCompaniesToVisit);
+        companyList.setAdapter(companyToVisitAdapter);
+
+        String[] namesOfFavorites = {"Volvo", "Ericsson"};
+        ListView companyListOfFav = (ListView)root.findViewById(R.id.listOfFavorites);
+        ArrayAdapter<String> favoriteCompanyAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,namesOfFavorites);
+        companyListOfFav.setAdapter(favoriteCompanyAdapter);
         return root;
     }
 }
