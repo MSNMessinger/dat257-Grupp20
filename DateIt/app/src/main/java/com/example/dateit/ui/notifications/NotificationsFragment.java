28c232a6cc0a4bf8ee8528794564cb16478ace4e
Tina Samimian <tina.samimian@gmail.com>
2020-04-23 11:31:46 +0200

    working scroll and height of listview adapted

--- DateIt/app/src/main/java/com/example/dateit/ui/notifications/NotificationsFragment.java
+++ DateIt/app/src/main/java/com/example/dateit/ui/notifications/NotificationsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -33,20 +34,47 @@ public class NotificationsFragment extends Fragment {
             }
         });*/
 
-        String[] namesOfCompaniesToVisit = {"Volvo", "Ericsson", "Brocoli"};
+        String[] namesOfCompaniesToVisit = {"Volvo", "Ericsson", "Brocoli","Volvo", "Ericsson", "Brocoli","Volvo", "Ericsson", "Brocoli"};
         ListView companyList = (ListView)root.findViewById(R.id.listToVisit);
         ArrayAdapter<String> companyToVisitAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,namesOfCompaniesToVisit);
         companyList.setAdapter(companyToVisitAdapter);
+        getTotalHeightofListView(companyList);
 
         String[] namesOfFavorites = {"SKF", "Spotify"};
         ListView companyListOfFav = (ListView)root.findViewById(R.id.listOfFavorites);
         ArrayAdapter<String> favoriteCompanyAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,namesOfFavorites);
         companyListOfFav.setAdapter(favoriteCompanyAdapter);
+        getTotalHeightofListView(companyListOfFav);
 
         String[] namesOfNotes = {"SKF", "Spotify", "Ericsson"};
         ListView companyListOfNotes = (ListView)root.findViewById(R.id.listOfNotes);
         ArrayAdapter<String> notesCompanyAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,namesOfNotes);
         companyListOfNotes.setAdapter(notesCompanyAdapter);
+        getTotalHeightofListView(companyListOfNotes);
         return root;
+    }
+
+    public static void getTotalHeightofListView(ListView listView) {
+
+        ListAdapter mAdapter = listView.getAdapter();
+
+        int totalHeight = 0;
+
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            View mView = mAdapter.getView(i, null, listView);
+
+            mView.measure(
+                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+
+                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+
+            totalHeight += mView.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight
+                + (listView.getDividerHeight() * (mAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
