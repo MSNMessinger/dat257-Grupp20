ced8aa4f36b72f5e90fc543038622d5e0bf96f3a
Norbert <norbert.laszlo98@gmail.com>
2020-04-27 13:43:44 +0200

    commented out 'Oscars fragment is ugly'

--- DateIt/app/src/main/java/com/example/dateit/ui/home/HomeViewModel.java
+++ DateIt/app/src/main/java/com/example/dateit/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Oscars fragment is ugly");
+        //mText.setValue("Oscars fragment is ugly");
     }
 
     public LiveData<String> getText() {
